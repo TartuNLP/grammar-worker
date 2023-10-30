dfd613a97542ad89f3c74451781be03f38e8c5f9
Agnes Luhtaru <agnes.luhtaru@ut.ee>
2023-10-30 11:09:29 +0200

    Added the choice between modular and nelb

--- gec_worker/gec.py
+++ gec_worker/gec.py
@@ -6,6 +6,7 @@ import warnings
 from .corrector import Corrector
 from .config import GECModelConfig
 from .modular_interface import ModularHubInterface
+from .nelb_interface import NELBHubInterface
 
 logger = logging.getLogger(__name__)
 
@@ -28,21 +29,31 @@ class GEC(Corrector):
         self.source_language = self.model_config.source_language
         self.target_language = self.model_config.target_language
 
-        self.max_positions = self.model.max_positions[0] if self.model_config.task == 'translation' else \
-        self.model.max_positions[f"{self.source_language}-{self.target_language}"][0]
+        self.max_positions = self.model.max_positions[0] #if self.model_config.task == 'translation' else \
+        #self.model.max_positions[f"{self.source_language}-{self.target_language}"][0]
 
         logger.info("GEC model loaded")
 
     def _load_model(self):
         sentencepiece_path = os.path.join(self.model_config.sentencepiece_dir, self.model_config.sentencepiece_prefix)
-        self.model = ModularHubInterface.from_pretrained(
+        if self.model_config.type == "nelb":
+            self.model = NELBHubInterface.from_pretrained(
             model_path=self.model_config.checkpoint,
             sentencepiece_prefix=sentencepiece_path,
-            truecase_model=self.model_config.truecase_model,
             dictionary_path=self.model_config.dict_dir,
             task=self.model_config.task,
             source_language=self.model_config.source_language,
             target_language=self.model_config.target_language)
+        else:
+            self.model = ModularHubInterface.from_pretrained(
+                model_path=self.model_config.checkpoint,
+                sentencepiece_prefix=sentencepiece_path,
+                truecase_model=self.model_config.truecase_model,
+                dictionary_path=self.model_config.dict_dir,
+                task=self.model_config.task,
+                source_language=self.model_config.source_language,
+                target_language=self.model_config.target_language)
+        
 
     def correct(self, sentences: List[str]) -> List[str]:
         return self.model.translate(sentences,
