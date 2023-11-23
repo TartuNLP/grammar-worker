4219fb507f02d6a156e609ea2d725ed782fe1f60
Jaagup Kippar <jaagup@tlu.ee>
2023-11-23 10:15:57 +0200

    Update __init__.py

--- gec_worker/__init__.py
+++ gec_worker/__init__.py
@@ -1,5 +1,6 @@
 from .config import *
 from .gec import GEC
 from .speller import Speller
+from .errorscorrection import ErrorsCorrection
 from .multi_corrector import MultiCorrector
 from .mq_consumer import MQConsumer
